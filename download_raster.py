22b5612ee59d5ae8b01774c810180ffb544afb44
xinwei89 <113005350+xinwei89@users.noreply.github.com>
2023-10-18 15:03:44 +1100

    added args option to choose directory

--- download_raster.py
+++ download_raster.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 
 import geopandas as gpd
 
@@ -10,9 +11,10 @@ def download(sa1_list, nsw_gdf, output_folder: str):
     for sa1 in sa1_list:
         print(sa1)
         sa1_gdf = nsw_gdf[nsw_gdf["SA1_CODE21"] == sa1]
-        sa1_image = SA1Image(sa1_gdf, 21)
-        sa1_image.save_as_full_geotiff(output_folder=f"{output_folder}_full")
-        sa1_image.save_as_sa1_geotiff(output_folder=f"{output_folder}_sa1_only")
+        if not sa1_gdf.empty:
+            sa1_image = SA1Image(sa1_gdf, 21)
+            sa1_image.save_as_full_geotiff(output_folder=f"{output_folder}_full")
+            sa1_image.save_as_sa1_geotiff(output_folder=f"{output_folder}_sa1_only")
 
 
 def main():
@@ -28,7 +30,12 @@ def main():
     # nsw_gdf = nsw_gdf[["SA1_CODE21", "xmin", "ymin", "xmax", "ymax", "geometry"]]
 
     # Don't download rasters that is already downloaded, so find them first and remove from nsw_gdf
-    root_directory = "."
+
+    if len(sys.argv) == 1:
+        root_directory = "."
+    else:
+        root_directory = sys.argv[1]
+
     extracted_sa1 = set()
 
     for root, dirs, files in os.walk(root_directory):
@@ -40,13 +47,13 @@ def main():
 
     extracted_sa1 = list(extracted_sa1)
     # sa1 with building annotations over 10%
-    nsw_gdf = gpd.read_file("filtered_nsw_sa1.geojson")
+    nsw_gdf = gpd.read_file(os.path.join(root_directory, "filtered_nsw_sa1.geojson"))
     nsw_gdf = nsw_gdf[~nsw_gdf["SA1_CODE21"].isin(extracted_sa1)]
 
     # Define the folder path to sa1 codes and respective percentage of building annotation coverage
     folder_path = "draft"
 
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(os.path.join(root_directory, folder_path)):
         if filename.startswith("sa1images") and filename.endswith(".txt"):
             file_path = os.path.join(folder_path, filename)
 
@@ -56,7 +63,9 @@ def main():
             download(
                 sa1_list,
                 nsw_gdf=nsw_gdf,
-                output_folder=f"coverage_{filename.split('.')[0]}",
+                output_folder=os.path.join(
+                    root_directory, f"coverage_{filename.split('.')[0]}"
+                ),
             )
 
 
